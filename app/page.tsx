0d4b4ed0134e3e904fd3393bf316771212397df5
kevincakey <57571360+kevincakey@users.noreply.github.com>
2024-06-20 06:05:35 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,12 @@
-import { createClient } from "@/utils/supabase/server"; // Adjust the import as necessary based on your project structure
-
-// Define the Todo type
-interface Todo {
-  id: string;
-  title: string;
-  completed: boolean;
-}
+import { createClient } from "@/utils/supabase/server";
+import Todo from "../types";
 
 export default async function Index() {
   const supabase = createClient();
 
   // Fetch data from the 'todos' table
-  const { data: todos, error } = await supabase.from("todos").select();
+  const { data: todos } = await supabase.from("todos").select();
 
-  // Check for errors
-  if (error) {
-    console.error("Error fetching todos list:", error);
-    return <div>Error loading todos.</div>;
-  }
-
-  // Ensure 'todos' is defined and an array
-  if (!todos) {
-    return <div>No todos found.</div>;
-  }
-
-  if (todos.length === 0) {
-    return <div>0 todos found.</div>;
-  }
-
-  // Render the list of todos
   return (
     <div>
       <h1>Todos List</h1>
